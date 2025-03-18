4820e2f2baee6fdb7f565daea5e7e7879fa0f604
MiguelAntuns7 <1241918@isep.ipp.pt>
2025-03-18 16:31:04 +0000

    Fixes #11: Deleted src folder, replaced it with Basic's src folder; copied webpack.config.js and package.json from the Basic folder to part3's folder; Recursively deleted the Built folder inside src/main/resources/static

---
+++ CA1/part3/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Entity // <1>
+public class Employee {
+
+	private @Id @GeneratedValue Long id; // <2>
+	private String firstName;
+	private String lastName;
+	private String description;
+	private String jobTitle;
+	private Integer jobYears;
+	private String email;
+
+	protected Employee() {}
+
+	public Employee(String firstName, String lastName, String description, String jobTitle, Integer jobYears, String email) {
+
+		validateParameters(firstName, lastName, description, jobTitle, jobYears, email);
+
+		setFirstName(firstName);
+		setLastName(lastName);
+		setDescription(description);
+		setJobTitle(jobTitle);
+		setJobYears(jobYears);
+		setEmail(email);
+	}
+
+	public void validateParameters (String firstName, String lastName, String description, String jobTitle, Integer jobYears, String email) {
+
+		if (firstName == null || firstName.isEmpty())
+			throw new IllegalArgumentException("First Name field must not be empty.");
+
+		if (lastName == null || lastName.isEmpty())
+			throw new IllegalArgumentException("Last Name field must not be empty.");
+
+		if (description == null || description.isEmpty())
+			throw new IllegalArgumentException("Description field must not be empty.");
+
+		if (jobTitle == null || jobTitle.isEmpty())
+			throw new IllegalArgumentException("Job Title field must not be empty.");
+
+		if (jobYears == null || jobYears < 0)
+			throw new IllegalArgumentException("Job Years field must be a positive value.");
+
+		if (email == null || !email.contains("@"))
+			throw new IllegalArgumentException("Email field must be a valid email.");
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) &&
+				Objects.equals(firstName, employee.firstName) &&
+				Objects.equals(lastName, employee.lastName) &&
+				Objects.equals(description, employee.description) &&
+				Objects.equals(jobTitle, employee.jobTitle) &&
+				Objects.equals(jobYears, employee.jobYears) &&
+				Objects.equals(email, employee.email);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(id, firstName, lastName, description, jobTitle, jobYears, email);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		if (firstName == null || firstName.isEmpty())
+			throw new IllegalArgumentException("First Name field must not be empty.");
+
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		if (lastName == null || lastName.isEmpty())
+			throw new IllegalArgumentException("Last Name field must not be empty.");
+
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		if (description == null || description.isEmpty())
+			throw new IllegalArgumentException("Description field must not be empty.");
+
+		this.description = description;
+	}
+
+	public String getJobTitle () {
+		return jobTitle;
+	}
+
+	public void setJobTitle (String jobTitle) {
+		if (jobTitle == null || jobTitle.isEmpty())
+			throw new IllegalArgumentException("Job Title field must not be empty.");
+
+		this.jobTitle = jobTitle;
+	}
+
+	public Integer getJobYears () {
+		return jobYears;
+	}
+
+	public void setJobYears (Integer jobYears) {
+		if (jobYears == null || jobYears < 0)
+			throw new IllegalArgumentException("Job Years field must be a positive value.");
+
+		this.jobYears = jobYears;
+	}
+
+	public String getEmail () {
+		return this.email;
+	}
+
+	public void setEmail (String email) {
+		if (email == null || !email.contains("@"))
+			throw new IllegalArgumentException("Email field must be a valid email.");
+
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", description='" + description + '\'' +
+				", jobTitle='" + jobTitle + '\'' +
+				", jobYears='" + jobYears + '\'' +
+				", email='" + email + '\'' +
+				'}';
+	}
+}
+// end::code[]
