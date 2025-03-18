263a0d2ed9e05ed4db2a6b19da2f3a9591cf4dcd
MiguelAntuns7 <1241918@isep.ipp.pt>
2025-03-18 15:43:41 +0000

    Extracted zipped spring boot project inside part3 folder

---
+++ CA1/part3/react-and-spring-data-rest-basic/src/main/java/com/greglturniquist/payroll/ReactAndSpringDataRestBasicApplication.java
@@ -0,0 +1,13 @@
+package com.greglturniquist.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestBasicApplication.class, args);
+	}
+
+}
